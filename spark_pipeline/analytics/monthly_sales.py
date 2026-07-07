20c25887948dd4f637d3512f361760c9de1e3278
sreerangakuntumalla21-dot <sreerangakuntumalla21@gmail.com>
2026-07-07 06:45:43 +0530

    Update monthly_sales.py

--- spark_pipeline/analytics/monthly_sales.py
+++ spark_pipeline/analytics/monthly_sales.py
@@ -3,14 +3,13 @@ from pyspark.sql.functions import month, year, sum
 
 def monthly_sales(orders, order_items):
 
-    # Join Orders and Order Items
+    
     sales = orders.join(
         order_items,
         "order_id"
     )
 
-    # Create monthly summary
-    monthly = (
+    
         sales
         .withColumn("Year", year("order_date"))
         .withColumn("Month", month("order_date"))
